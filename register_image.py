ca40c320cf918958bd6031506b3814d56c61bbd2
Scott McMichael <scott.t.mcmichael@nasa.gov>
2015-10-16 19:00:15 -0700

    Added option to take a reference image as input

--- register_image.py
+++ register_image.py
@@ -25,25 +25,29 @@ CONFIDENCE_NONE = 0
 CONFIDENCE_LOW  = 1
 CONFIDENCE_HIGH = 2
 
-def convertTransformToGeo(transform, newImagePath, refImagePath):
-    '''Converts an image-to-image homography to the ProjectiveTransform
-       class used elsewhere in geocam.'''
+CONFIDENCE_STRINGS = ['NONE', 'LOW', 'HIGH']
 
-    # Convert the transform parameters to a class
+def convertTransformToGeo(transform, newImagePath, refImagePath, refImageGeoTransform=None):
+    '''Converts an image-to-image homography to the ProjectiveTransform
+       class used elsewhere in geocam.
+       Either the reference image must be geo-registered, or the geo transform for it
+       must be provided.'''
+
+    # Convert the image-to-image transform parameters to a class
     temp = numpy.array([transform[0:3], transform[3:6], transform[6:9]] )
     imageToRefTransform = geocamTiePoint.transform.ProjectiveTransform(temp)
 
-    # Get metadata from the new and reference image
     newImageSize = ImageFetcher.miscUtilities.getImageSize(newImagePath)
-    refStats     = IrgGeoFunctions.getImageGeoInfo(refImagePath, False)
-    (minLon, maxLon, minLat, maxLat) = refStats['lonlat_bounds']
-    
-    # Make a transform for ref pixel to GDC
-    xScale = (maxLon - minLon) / refStats['image_size'][1]
-    yScale = (maxLat - minLat) / refStats['image_size'][0]
-    refPixelToGdcTransform = numpy.array([[xScale, 0,      minLon],
-                                          [0,      yScale, minLat],
-                                          [0 ,     0,      1     ]])
+
+    if not refImageGeoTransform:
+        # Make a transform from ref pixel to GDC using metadata on disk
+        refStats     = IrgGeoFunctions.getImageGeoInfo(refImagePath, False)
+        (minLon, maxLon, minLat, maxLat) = refStats['lonlat_bounds']
+        xScale = (maxLon - minLon) / refStats['image_size'][1]
+        yScale = (maxLat - minLat) / refStats['image_size'][0]
+        refPixelToGdcTransform = numpy.array([[xScale, 0,      minLon],
+                                              [0,      yScale, minLat],
+                                              [0 ,     0,      1     ]])
 
     # Generate a list of point pairs
     imagePoints = []
@@ -56,17 +60,23 @@ def convertTransformToGeo(transform, newImagePath, refImagePath):
         for c in range(0, newImageSize[1], pointPixelSpacing):
             thisPixel = numpy.array([float(c), float(r)])
             pixelInRefImage = imageToRefTransform.forward(thisPixel)
-            temp = numpy.array(list(pixelInRefImage) + [1], dtype='float64') # Homogenize the input point
-            gdcCoordinate   = refPixelToGdcTransform.dot(temp)[0:2]
-            projectedCoordinate = geocamTiePoint.transform.lonLatToMeters(gdcCoordinate)
+            homogPixel = numpy.array(list(pixelInRefImage) + [1], dtype='float64') # Homogenize the input point
+            
+            if (not refImageGeoTransform):
+                # Use the geo information of the 
+                gdcCoordinate   = refPixelToGdcTransform.dot(homogPixel)[0:2]
+                projectedCoordinate = geocamTiePoint.transform.lonLatToMeters(gdcCoordinate)
+            else: # Use the user-provided transform
+                projectedCoordinate = refImageGeoTransform.forward(homogPixel)
+                
             imagePoints.append(thisPixel)
             worldPoints.append(projectedCoordinate)
             #print str(thisPixel) + ' --> ' + str(gdcCoordinate) + ' <--> ' + str(projectedCoordinate)
 
     # Compute a transform object that converts from the new image to projected coordinates
-    print 'Converting transform to world coordinates...'
+    #print 'Converting transform to world coordinates...'
     outputTransform = geocamTiePoint.transform.getTransform(numpy.asarray(worldPoints),
-                                                            numpy.asarray(imagePoints))    
+                                                            numpy.asarray(imagePoints))
     
     #print outputTransform   
     #for i, w in zip(imagePoints, worldPoints):
@@ -106,10 +116,18 @@ def alignImages(testImagePath, refImagePath, workPrefix, force):
     
     # Run the C++ command if we need to generate the transform
     if (not os.path.exists(transformPath) or force):
+        if os.path.exists(transformPath):
+            os.remove(transformPath) # Clear out any old results
         cmd = ['build/registerGeocamImage', refImagePath, testImagePath, transformPath, '--debug']
-        print cmd
+        #print cmd
         p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
         textOutput, err = p.communicate()
+    
+    if not os.path.exists(transformPath):
+        #raise Exception('Failed to compute transform!')
+        transform = [1, 0, 0, 0, 1, 0, 0, 0, 1]
+        confidence = CONFIDENCE_NONE
+        return (transform, confidence)
     
     # Load the computed transform and confidence
     handle   = open(transformPath, 'r')
@@ -132,27 +150,32 @@ def alignImages(testImagePath, refImagePath, workPrefix, force):
 # Main interface function
 
 
-def register_image(imagePath, centerLon, centerLat, focalLength, imageDate):
+def register_image(imagePath, centerLon, centerLat, focalLength, imageDate,
+                   refImagePath=None, referenceGeoTransform=None):
     '''Attempts to geo-register the provided image.
        Returns a transform from image coordinates to projected meters coordinates.
        Also returns an evaluation of how likely the registration is to be correct.'''
 
     # Set up paths in a temporary directory
     workDir = tempfile.mkdtemp()
-    refImagePath = os.path.join(workDir, 'ref_image.tif')
     workPrefix = workDir + '/work-'
     
-    # Fetch the reference image
-    estimatedMpp = estimateGroundResolution(focalLength)
-    return ImageFetcher.fetchReferenceImage.fetchReferenceImage(centerLon, centerLat,
-                                                                estimatedMpp, imageDate, refImagePath)
+    if not referenceImage:
+        # Fetch the reference image
+        estimatedMpp = estimateGroundResolution(focalLength)
+        refImagePath = os.path.join(workDir, 'ref_image.tif')
+        ImageFetcher.fetchReferenceImage.fetchReferenceImage(centerLon, centerLat,
+                                                             estimatedMpp, imageDate, refImagePath)
+    else: # The user provided a reference image
+        if not os.path.exists(refImagePath):
+            raise Exception('Provided reference image path does not exist!')
 
     # Try to align to the reference image
     force = True
     (transform, confidence) = alignImages(imagePath, refImagePath, workPrefix, force)
 
     # Convert the transform into a pixel-->Projected coordinate transform
-    geoTransform = register_image.convertTransformToGeo(transform, testImagePath, refImagePath)
+    geoTransform = register_image.convertTransformToGeo(transform, testImagePath, refImagePath, referenceGeoTransform)
 
     return (geoTransform, confidence)
 
