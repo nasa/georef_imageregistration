ec95b2269da7aa2f7df649266c7340a7b5355275
Grace Lee <yeongrace.lee@gmail.com>
2016-01-21 18:54:07 +0000

    minor changes to test_registration

--- test_registration.py
+++ test_registration.py
@@ -11,6 +11,7 @@ import ImageFetcher.miscUtilities
 import ImageFetcher.fetchReferenceImage
 import register_image
 import IrgStringFunctions, IrgGeoFunctions
+from __builtin__ import True
 
 # from geocamTiePoint import transform
 
@@ -45,23 +46,22 @@ def readTestInfo():
     '''Read in the test data set information from a file'''
     
     testData = []
-    testData.append(TestInstance( 'ISS011-E-7860.JPG,   59.5,  46.0,   56.3,   48.0, 180, 2005.06.03' )) # GOOD
-#     testData.append(TestInstance( 'ISS017-E-9641.JPG,  -92.5,  45.9,  -91.4,   45.9, 400, 2008.06.18' )) # Basemap too low res?
-#     testData.append(TestInstance( 'ISS006-E-30169.JPG,-104.0,  44.0, -113.2,   48.9, 50, 2003.02.20' ))
-#     testData.append(TestInstance( 'ISS002-386-32.JPG, -108.0,  36.5, -110.3,   36.5, 110, 2001.07.08' ))
-#     testData.append(TestInstance( 'ISS004-E-8271.JPG,  -77.0,  42.0,    0.0,    0.0, 400, 2002.02.26' ))
-#     testData.append(TestInstance( 'ISS004-E-6686.JPG, -146.0, -16.0, -144.8,  -17.0, 400, 2002.01.22' ))
-#     testData.append(TestInstance( 'ISS004-E-5201.JPG, -113.0,  32.5,    0.0,    0.0, 0' ))
-#     testData.append(TestInstance( 'ISS005-E-16101.JPG, -67.0, -14.0,  -67.2,  -12.6, 180, 2002.09.26' ))
-#     testData.append(TestInstance( 'ISS005-E-13496.JPG,  -5.4,  50.4,   -2.3,   50.4, 400, 2002.09.10' ))
-#     testData.append(TestInstance( 'ISS006-E-34067.JPG, -81.0,  51.0,  -81.8,   51.5, 50, 2003.02.26' ))
+#     testData.append(TestInstance( 'ISS011-E-7860.JPG,   59.5,  46.0,   56.3,   48.0, 180, 2005.06.03' )) # LOW
+#     testData.append(TestInstance( 'ISS017-E-9641.JPG,  -92.5,  45.9,  -91.4,   45.9, 400, 2008.06.18' )) # NONE
+#     testData.append(TestInstance( 'ISS006-E-30169.JPG,-104.0,  44.0, -113.2,   48.9, 50, 2003.02.20' )) # LOW
+#     testData.append(TestInstance( 'ISS002-386-32.JPG, -108.0,  36.5, -110.3,   36.5, 110, 2001.07.08' )) # LOW
+#     testData.append(TestInstance( 'ISS004-E-8271.JPG,  -77.0,  42.0,    0.0,    0.0, 400, 2002.02.26' )) # NONE
+#     testData.append(TestInstance( 'ISS004-E-6686.JPG, -146.0, -16.0, -144.8,  -17.0, 400, 2002.01.22' )) # LOW
+    testData.append(TestInstance( 'ISS005-E-16101.JPG, -67.0, -14.0,  -67.2,  -12.6, 180, 2002.09.26' )) # ***HIGH!***
+#     testData.append(TestInstance( 'ISS005-E-13496.JPG,  -5.4,  50.4,   -2.3,   50.4, 400, 2002.09.10' )) # NONE
+#     testData.append(TestInstance( 'ISS006-E-34067.JPG, -81.0,  51.0,  -81.8,   51.5, 50, 2003.02.26' )) # LOW
 #     testData.append(TestInstance( 'ISS006-E-21717.JPG, 114.0,   9.0,  112.5,    7.5, 340, 2003.01.31' ))
 #     testData.append(TestInstance( 'ISS006-E-9482.JPG,  -58.0,  49.0,  -62.0,   48.9, 400, 2002.12.17' ))
 #     testData.append(TestInstance( 'ISS010-E-6710.JPG,  116.5, -23.0,  117.90, -23.9, 180, 2004.11.12' ))
 #     testData.append(TestInstance( 'ISS012-E-19064.JPG, -71.0,  41.5,  -68.2,   42.6, 800, 2006.03.04' ))
-    testData.append(TestInstance( 'ISS012-E-8578.JPG,  -80.0,  33.5,  -80.3,   31.5, 180, 2005.11.19' )) # GOOD
+#     testData.append(TestInstance( 'ISS012-E-8578.JPG,  -80.0,  33.5,  -80.3,   31.5, 180, 2005.11.19' )) # GOOD
 #     testData.append(TestInstance( 'ISS012-E-7953.JPG,  -74.0,  41.4,  -72.8,   42.3, 400, 2005.11.14' ))
-    testData.append(TestInstance( 'ISS013-E-6881.JPG,  -74.1,  22.7,  -72.7,   24.9, 800, 2006.04.12' )) # GOOD
+#     testData.append(TestInstance( 'ISS013-E-6881.JPG,  -74.1,  22.7,  -72.7,   24.9, 800, 2006.04.12' )) # GOOD
 #     testData.append(TestInstance( 'ISS017-E-18969.JPG, -74.8,  45.0,  -74.3,   48.5, 800, 2008.10.10' ))
     #testData.append(TestInstance( 'STS066-117-11.JPG, -143.5, 62.0, -144.0, 57.1, 100, 1994.11.09' ))
     
@@ -89,6 +89,8 @@ def runTest(test, options):
     testImagePath = test.imagePath
     lon = test.imageCenterLoc[0]
     lat = test.imageCenterLoc[1]
+    print "lon %d" % lon
+    print "lat %d" % lat
 
     # Set up paths
     imageBase    = os.path.splitext(testImagePath)[0]
@@ -104,6 +106,8 @@ def runTest(test, options):
         print 'Fetching reference image for input ' + testImagePath
             
         estimatedMpp = register_image.estimateGroundResolution(test.focalLength)
+        print "estimatedMpp %d" % estimatedMpp 
+        print "focalLength %d" % test.focalLength
         ImageFetcher.fetchReferenceImage.fetchReferenceImage(test.imageCenterLoc[0], test.imageCenterLoc[1],
                                                              estimatedMpp, test.date, refImagePath)
 
@@ -115,7 +119,9 @@ def runTest(test, options):
     # TODO: Don't care about the tranform path!
     transformPath = workPrefix + '-transform.txt'
     force = not options.useExisting
-    (tform, confidence) = register_image.alignImages(testImagePath, refImagePath, workPrefix, force)
+    debug = True
+    slowMethod = True
+    (tform, confidence, testInliers, refInliers) = register_image.alignImages(testImagePath, refImagePath, workPrefix, force, debug, slowMethod)
     if confidence == register_image.CONFIDENCE_NONE:
         raise Exception('Failed to register image!')
 
@@ -198,6 +204,7 @@ def main():
     testInfo = readTestInfo()
     confidenceCounts = [0, 0, 0]
     results = []
+    geoTransform = None
     for i in testInfo:
         try:
             (score, confidence, geoTransform) = runTest(i, options)
@@ -211,6 +218,7 @@ def main():
         
         confidenceCounts[confidence] += 1
         print i.imagePath + ' ---> ' + str(score) + ' == ' + register_image.CONFIDENCE_STRINGS[confidence]
+        
         print "transform"
         print geoTransform
 
@@ -222,3 +230,4 @@ def main():
 
 if __name__ == "__main__":
     sys.exit(main())
+ 
