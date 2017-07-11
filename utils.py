c542371eb23ae52fd6e33f7a4db04f39a99a5487
Grace Lee <grace.lee@nasa.gov>
2017-07-11 21:17:06 +0000

    new licenses added

--- utils.py
+++ utils.py
@@ -1,9 +1,9 @@
 #__BEGIN_LICENSE__
-# Copyright (c) 2015, United States Government, as represented by the
+# Copyright (c) 2017, United States Government, as represented by the
 # Administrator of the National Aeronautics and Space Administration.
 # All rights reserved.
 #
-# The xGDS platform is licensed under the Apache License, Version 2.0
+# The GeoRef platform is licensed under the Apache License, Version 2.0
 # (the "License"); you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 # http://www.apache.org/licenses/LICENSE-2.0.
